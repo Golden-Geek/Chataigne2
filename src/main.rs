99f06f4fcbaf0764bd8225ecf2605d77ddd900f0
Ben Kuper <bkuperberg@gmail.com>
2026-02-13 15:35:33 +0100

    from scratch again, giant enum approach

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,12 @@
+use golden_core::{
+    define_node_enum, engine::Engine, node::Container
+};
+
+define_node_enum!(enum ChataigneNode {
+    // App-specific node types go here.
+});
+
 fn main() {
-    println!("Hello, world!");
-    // test golden_core
+    let root: ChataigneNode = Container::new("Root".to_string()).into();
+    let _engine = Engine::<ChataigneNode>::new(root);
 }
