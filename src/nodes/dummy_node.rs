aab6fec98f3ddd923c3f7d6433c245d9e96903c3
Ben Kuper <bkuperberg@gmail.com>
2026-02-15 12:58:21 +0100

    callbacks for macros

--- src/nodes/dummy_node.rs
+++ src/nodes/dummy_node.rs
@@ -45,7 +45,7 @@ impl Node for DummyNode {
 #[node]
 #[params(
 folder(output, label = "Output", reuse = true) {
-    very_dummy_param: f64 = 2.2 [0.0..10.0] (label = "Very Dummy Param", description = "A parameter stored on a node using the #[param] attribute macro");
+    very_dummy_param: f64 = 2.2 [0.0..10.0] (label = "Very Dummy Param", description = "A parameter stored on a node using the #[param] attribute macro", default_callback);
 }
 )]
 pub struct VeryDummyNode {
@@ -56,6 +56,15 @@ impl VeryDummyNode {
     pub fn create(label: impl Into<String>) -> Self {
         let label = label.into();
         Self::new(label.clone(), DummyNode::new(label))
+    }
+
+    fn on_very_dummy_param_change(&mut self, _ctx: &mut ProcessCtx, _old_value: ParamValue) {
+        if cfg!(debug_assertions) {
+            // println!(
+                // "VeryDummyNode very_dummy_param changed: old={old_value:?} new={}",
+                // self.very_dummy_param.get()
+            // );
+        }
     }
 }
 
@@ -71,44 +80,57 @@ impl Node for VeryDummyNode {
         self.dummy.update(ctx);
     }
 
-    fn on_param_change(&mut self, _ctx: &mut ProcessCtx, node_id: NodeId, old_value: ParamValue) {
-        if node_id == self.very_dummy_param.id() {
-            // println!("VeryDummyNode very_dummy_param changed: old={:?} new={}", old_value, self.very_dummy_param.get());
-        }
+    fn on_param_change(&mut self, _ctx: &mut ProcessCtx, node_id: NodeId, _old_value: ParamValue) {
         if node_id == self.dummy.dummy_param.id() {
-            // println!("VeryDummyNode dummy_param changed: old={:?} new={}", old_value, self.dummy.dummy_param.get());
+            if cfg!(debug_assertions) {
+                // println!(
+                //     "VeryDummyNode dummy_param changed: old={old_value:?} new={}",
+                //     self.dummy.dummy_param.get()
+                // );
+            }
         }
     }
 }
 
-#[node]
-pub struct SuperDummyNode {
-    very_dummy: VeryDummyNode,
+// #[node]
+// pub struct SuperDummyNode {
+//     very_dummy: VeryDummyNode,
 
-    #[param(default = 0.25, min = 0.0, max = 1.0, label = "Super Dummy Param", description = "A parameter stored on a node using via composition")]
-    super_dummy_param: ParameterHandle<f64>,
-}
+//     #[param(default = 0.25, min = 0.0, max = 1.0, label = "Super Dummy Param", description = "A parameter stored on a node using via composition")]
+//     super_dummy_param: ParameterHandle<f64>,
+// }
 
-impl SuperDummyNode {
-    pub fn create(label: impl Into<String>) -> Self {
-        let label = label.into();
-        Self::new(label.clone(), VeryDummyNode::create(label))
-    }
-}
+// impl SuperDummyNode {
+//     pub fn create(label: impl Into<String>) -> Self {
+//         let label = label.into();
+//         Self::new(label.clone(), VeryDummyNode::create(label))
+//     }
+// }
 
-#[node(via = very_dummy, from_struct)]
-impl Node for SuperDummyNode {
-    fn init(&mut self, _ctx: &mut ProcessCtx) {
-        println!("SuperDummyNode init, super_dummy_param initial value: {}", self.super_dummy_param.get());
-        println!("SuperDummyNode init, very_dummy_param initial value: {}", self.very_dummy.very_dummy_param.get());
-    }
+// #[node(via = very_dummy, from_struct)]
+// impl Node for SuperDummyNode {
+//     fn init(&mut self, _ctx: &mut ProcessCtx) {
+//         println!("SuperDummyNode init, super_dummy_param initial value: {}", self.super_dummy_param.get());
+//         println!("SuperDummyNode init, very_dummy_param initial value: {}", self.very_dummy.very_dummy_param.get());
+//     }
 
-    fn on_param_change(&mut self, _ctx: &mut ProcessCtx, node_id: NodeId, old_value: ParamValue) {
-        if node_id == self.super_dummy_param.id() {
-            // println!("SuperDummyNode super_dummy_param changed: old={:?} new={}", old_value, self.super_dummy_param.get());
-        }
-        if node_id == self.very_dummy.very_dummy_param.id() {
-            // println!("SuperDummyNode very_dummy_param changed: old={:?} new={}", old_value, self.very_dummy.very_dummy_param.get());
-        }
-    }
-}
+//     fn on_param_change(&mut self, _ctx: &mut ProcessCtx, node_id: NodeId, _old_value: ParamValue) {
+        
+//         match node_id {
+//             id if id == self.super_dummy_param.id() => {
+//                 let _new_value = self.super_dummy_param.get();
+//                 if cfg!(debug_assertions) {
+//                     // println!("SuperDummyNode super_dummy_param changed: old={:?} new={new_value}", old_value);
+//                 }
+//             },
+//             id if id == self.very_dummy.very_dummy_param.id() => {
+//                 let _new_value = self.very_dummy.very_dummy_param.get();
+//                 if cfg!(debug_assertions) {
+//                     // println!("SuperDummyNode very_dummy_param changed: old={:?} new={new_value}", old_value);
+//                 }
+//             },
+//             _ => {},
+//         }
+        
+//     }
+// }
