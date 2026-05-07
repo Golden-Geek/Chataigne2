4ff1772735218198728f371caf4adc859a85aa9a
Ben Kuper <bkuperberg@gmail.com>
2026-05-07 18:14:26 +0200

    category icons

--- src-ui/src/lib/assets/icons/node-icons.svelte.ts
+++ src-ui/src/lib/assets/icons/node-icons.svelte.ts
@@ -1,10 +1,22 @@
 import type { NodeIconSet } from 'golden_ui';
 import moduleIcon from './nodes/module.svg';
 import moduleManagerIcon from './nodes/module_manager.svg';
+import moduleCategoryNetworkIcon from './module/network.svg';
+import moduleCategoryHardwareIcon from './module/hardware.svg';
+import moduleCategoryAudioIcon from './module/audio.svg';
+import moduleCategoryVideoIcon from './module/video.svg';
+import moduleCategorySystemIcon from './module/system.svg';
 
 export const appIcons: NodeIconSet = {
 	nodeTypes: {
 		module_manager: moduleManagerIcon,
 		module: moduleIcon
+	},
+	categories: {
+		Network: moduleCategoryNetworkIcon,
+		Hardware: moduleCategoryHardwareIcon,
+		Audio: moduleCategoryAudioIcon,
+		Video: moduleCategoryVideoIcon,
+		System: moduleCategorySystemIcon
 	}
 };
